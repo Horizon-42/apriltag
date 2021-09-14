c06a2aee449f9db56bf70a00cd47eeffe700541d
horizon <liudxp@live.cn>
2021-09-14 22:33:20 +0800

    update

--- detect_apriltag.go
+++ detect_apriltag.go
@@ -1,17 +1,44 @@
-package apriltag
+package go_apriltag
 
 /*
+#cgo CPPFLAGS: -I /usr/local/include -I /usr/local/opencv4/include/opencv4 -I /usr/local/include/apriltag -I /usr/local/include/apriltag/common
+#cgo LDFLAGS: -L /usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_objdetect -lopencv_dnn -lapriltag
+#include <stdlib.h>
 #include "detect_apriltag.h"
 */
 import "C"
-import "gocv.io/x/gocv"
+import (
+	"fmt"
+	"gocv.io/x/gocv"
+)
+
+type Mat struct {
+	p C.Mat
+}
+
+func (m *Mat) MatSetP(mat gocv.Mat) {
+	m.p = C.Mat(mat.Ptr())
+}
+
+func (m *Mat) Ptr() C.Mat {
+	return m.p
+}
 
 func Init(decimate, blur float32, threads int) bool {
 	return bool(C.Init(C.float(decimate), C.float(blur), C.int(threads)))
 }
 
 func HaveAprilTags(frame gocv.Mat) int {
-	return int(C.HaveAprilTags(frame.Ptr()))
+	src := Mat{}
+	src.MatSetP(frame)
+	return int(C.HaveAprilTags(src.Ptr()))
+}
+
+func DetectAprilTags(frame gocv.Mat) int {
+	src := Mat{}
+	src.MatSetP(frame)
+	fmt.Println(frame.Size())
+	return int(C.DetectAprilTags(src.Ptr()))
 }
 
 func Close() {
--- detect_apriltag.h
+++ detect_apriltag.h
@@ -5,29 +5,32 @@
 #ifndef APRILTAG_DETECT_APRILTAG_H
 #define APRILTAG_DETECT_APRILTAG_H
 
+#include <stdbool.h>
+
 #ifdef __cplusplus
 #include <opencv2/opencv.hpp>
-
+typedef cv::Mat* Mat;
 using namespace std;
-using namespace cv;
-
-extern "C" {
+#else
+typedef void *Mat;
 #endif
 
-#include "apriltag.h"
-#include "tag36h11.h"
-#include "tag25h9.h"
-#include "tag16h5.h"
-#include "tagCircle21h7.h"
-#include "tagCircle49h12.h"
-#include "tagCustom48h12.h"
-#include "tagStandard41h12.h"
-#include "tagStandard52h13.h"
+#ifdef __cplusplus
+extern "C" {
+#include "apriltags/TagDetector.h"
+#include "apriltags/Tag16h5.h"
+#include "apriltags/Tag25h7.h"
+#include "apriltags/Tag25h9.h"
+#include "apriltags/Tag36h9.h"
+#include "apriltags/Tag36h11.h"
 #include "common/getopt.h"
+#endif
 
 bool Init(float decimate, float blur, int threads);
 
-int HaveAprilTags(Mat const &frame);
+int HaveAprilTags(Mat frame);
+
+int DetectAprilTags(Mat frame);
 
 void Close();
 
